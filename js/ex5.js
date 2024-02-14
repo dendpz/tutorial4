fdd654e9a2fbb4fe4bac3aa7d97ec316206d1e09
Dennis <depazdennis@gmail.com>
2024-02-14 05:20:25 -0800

    ex5 v1

--- js/ex5.js
+++ js/ex5.js
@@ -3,3 +3,22 @@
 */
 
 console.log("De Paz's Output from Tutorial 4 Password");
+const correctPassword = "secret";
+let i = 1
+
+while (i <=3) {
+  const userInput = prompt("Please enter your password, you have 3 attempts:");
+  if (userInput != correctPassword) {
+    alert("Incorrect password, please try again:");
+    console.log("Incorrect password, please try again:");
+    i++;
+  }
+  else if (userInput === correctPassword) {
+    i++;
+    alert(`You have entered the correct password after ${i} attempt(s)`);
+    console.log(`You have entered the correct password after ${i} attempt(s)`);
+  }
+  else {
+    alert("Your account is locked! You have failed to enter the correct password 3 times.");
+  }
+}
