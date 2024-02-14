32aafed72d6c6b7cd01dc7b2d3c76fe2dabf9f71
Dennis <depazdennis@gmail.com>
2024-02-14 04:15:45 -0800

    ex3 c3

--- js/ex3.js
+++ js/ex3.js
@@ -3,3 +3,44 @@
 */
 
 console.log("De Paz's Output from Tutorial 4 Following Day");
+
+const dayEntered = prompt("Please enter the 3 letter abbreviation of the day in lower case:")
+switch (dayEntered) {
+  case "mon":
+    let followingDay = "tue";
+    alert(`You entered: ${dayEntered}\nThe following day is: ${followingDay}`);
+    console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay}`);
+    break
+  case "tue":
+    let followingDay1 = "wed";
+    alert(`You entered: ${dayEntered}\nThe following day is: ${followingDay1}`);
+    console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay1}`);
+    break
+  case "wed":
+    let followingDay2 = "thu"
+    alert(`You entered: ${dayEntered}\nThe following day is: ${followingDay2}`);
+    console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay2}`);
+    break
+  case "thu":
+    let followingDay3 = "fri"
+    alert(`You entered: ${dayEntered}\nThe following day is: ${followingDay3}`);
+    console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay3}`);
+    break
+  case "fri":
+    let followingDay4 = "sat"
+    alert(`You entered: ${dayEntered}\nThe following day is: ${followingDay4}`);
+    console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay4}`);
+    break
+  case "sat":
+    let followingDay5 = "sun"
+    alert(`You entered: ${dayEntered}\nThe following day is: ${followingDay5}`);
+    console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay5}`);
+    break
+  case "sun":
+    let followingDay6 = "mon"
+    alert(`You entered: ${dayEntered}\nThe following day is: ${followingDay6}`);
+    console.log(`You entered: ${dayEntered}\nThe following day is: ${followingDay6}`);
+    break
+  default:
+    alert("Not a valid entry")
+}
