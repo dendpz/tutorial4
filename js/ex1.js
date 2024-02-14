fed07791c9918ff9ebea6f977ffd72657fcd2b27
Dennis <depazdennis@gmail.com>
2024-02-14 02:51:42 -0800

    Ex2 update 2

--- js/ex1.js
+++ js/ex1.js
@@ -5,8 +5,10 @@
 console.log("De Paz's Output from Tutorial 4 Example 1");
 const userNumber = Number(prompt("Enter a number between 1-100:"));
 if ((userNumber > 1) && (userNumber < 100)) {
+   alert(`Thank you! You entered ${userNumber}, a valid number.`);
    console.log(`Thank you! You entered ${userNumber}, a valid number.`);
 }
 else {
-   console.log(`Sorry, ${userNumber} is not a valid entry.`)
+   alert(`Sorry, ${userNumber} is not a valid entry.`);
+   console.log(`Sorry, ${userNumber} is not a valid entry.`);
 }
