9a20e405f71782deba8fb37127a3e94a4f352af3
Dennis <depazdennis@gmail.com>
2024-02-14 05:57:22 -0800

    ex5 fin.1

--- js/ex5.js
+++ js/ex5.js
@@ -4,11 +4,12 @@
 
 console.log("De Paz's Output from Tutorial 4 Password");
 const correctPassword = "secret";
-let i = 1
+let i = 0
 
 while (i <=3) {
   const userInput = prompt("Please enter your password, you have 3 attempts:");
-  if (userInput != correctPassword) {
+  if(userInput != correctPassword) {
+    i++;
     if (i>3) {
       alert("Your account is locked! You have failed to enter the correct password 3 times.");
       console.log("Your account is locked! You have failed to enter the correct password 3 times.")
@@ -16,10 +17,10 @@ while (i <=3) {
     else {
       alert("Incorrect password, please try again:");
       console.log("Incorrect password, please try again:");
-      i++;
     }
   }
   else if (userInput === correctPassword) {
+    i++
     alert(`You have entered the correct password after ${i} attempt(s)`);
     console.log(`You have entered the correct password after ${i} attempt(s)`);
     break
