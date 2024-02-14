e7d28392b105096fa0cb85b231e27674cb253201
Dennis <depazdennis@gmail.com>
2024-02-14 04:35:19 -0800

    ex4 final

--- js/ex4.js
+++ js/ex4.js
@@ -4,3 +4,14 @@
 
 console.log("De Paz's Output from Tutorial 4 Simple Calculation");
 
+const value1 = Number(prompt("Please enter the first integer:"));
+const value2 = Number(prompt("Please enter the second integer:"));
+
+if ((Number.isInteger(value1) && (Number.isInteger(value2)))) {
+  alert(value1 + value2, value1 - value2, value1 * value2, value1 / value2, value1 % value2);
+  console.log(value1 + value2, value1 - value2, value1 * value2, value1 / value2, value1 % value2);
+}
+else {
+  alert("That is an incorrect input!")
+  console.log("That is an incorrect input!")
+}
