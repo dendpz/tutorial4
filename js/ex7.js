9d431a7175488ee49304a37b0bb0939a178e8226
Dennis <depazdennis@gmail.com>
2024-02-14 15:33:24 -0800

    ex7 v6

--- js/ex7.js
+++ js/ex7.js
@@ -4,13 +4,39 @@
 
 console.log("De Paz's Output from Tutorial 4 Following Second");
 
-const hours = Number(prompt("Enter the hour(s):"));
-const minutes = Number(prompt("Enter the minute(s):"));
-const seconds = Number(prompt("Enter the second(s):"));
+let hours = Number(prompt("Enter the hour(s):"));
+let minutes = Number(prompt("Enter the minute(s):"));
+let seconds = Number(prompt("Enter the second(s):"));
+let secondsZero = 0
+let minutesZero = 0
+let hoursZero = 0
+let secondAfter = seconds + 1
+let minutesAfter = minutes + 1
+let hoursAfter = hours + 1
 
 if ((Number.isInteger(hours)) && (Number.isInteger(minutes)) && (Number.isInteger(seconds))) {
-  alert(`${hours}h${minutes}m${seconds}s`);
-  console.log(`${hours}h${minutes}m${seconds}s`);
+  if (((seconds === 59) && (minutes != 59))) {
+    alert(`Time input: ${hours}h${minutes}m${seconds}s`);
+    console.log(`Time input: ${hours}h${minutes}m${seconds}s`);
+    alert(`One second later: ${hours}h${minutesAfter}m${secondsZero}`);
+    console.log(`One second later: ${hours}h${minutesAfter}m${secondsZero}`);
+  }
+  else if (((seconds === 59) && (minutes === 59) && (hours != 23))) {
+    alert(`Time input: ${hours}h${minutes}m${seconds}s`);
+    console.log(`Time input: ${hours}h${minutes}m${seconds}s`);
+    alert(`One second later: ${hoursAfter}h${minutesZero}m${secondsZero}`);
+    console.log(`One second later: ${hoursAfter}h${minutesZero}m${secondsZero}`);
+  }
+  else if (((seconds === 59) && (minutes === 59) && (hours === 23 ))) {
+    alert(`Time input: ${hours}h${minutes}m${seconds}s`);
+    console.log(`Time input: ${hours}h${minutes}m${seconds}s`);
+    alert(`One second later: ${hoursZero}h${minutesZero}m${secondsZero}`);
+    console.log(`One second later: ${hoursZero}h${minutesZero}m${secondsZero}`);
+  }
+  else {
+    alert(`Time input: ${hours}h${minutes}m${seconds}s`);
+    console.log(`Time input: ${hours}h${minutes}m${seconds}s`);
+  }
 }
 else {
   alert("You did not enter a number!")
