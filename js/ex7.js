bbe4e2d57246453d93c45895007e263ec907b89d
Dennis <depazdennis@gmail.com>
2024-02-14 14:04:49 -0800

    ex7 v4

--- js/ex7.js
+++ js/ex7.js
@@ -8,7 +8,7 @@ const hours = Number(prompt("Enter the hour(s):"));
 const minutes = Number(prompt("Enter the minute(s):"));
 const seconds = Number(prompt("Enter the second(s):"));
 
-if ((Number.isInteger(hours)) && (number.isInteger(minutes)) && (Number.isInteger(seconds))) {
+if ((Number.isInteger(hours)) && (Number.isInteger(minutes)) && (Number.isInteger(seconds))) {
   alert(`Here is what you entered: ${hours+minutes+seconds}`);
   console.log("Here is your number")
 }
