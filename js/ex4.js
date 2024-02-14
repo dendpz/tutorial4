73d2d37dfabf3ff152399879c0df4972765e9ff4
Dennis <depazdennis@gmail.com>
2024-02-14 04:42:20 -0800

    ex final.2

--- js/ex4.js
+++ js/ex4.js
@@ -8,7 +8,11 @@ const value1 = Number(prompt("Please enter the first integer:"));
 const value2 = Number(prompt("Please enter the second integer:"));
 
 if ((Number.isInteger(value1) && (Number.isInteger(value2)))) {
-  alert(value1 + value2, value1 - value2, value1 * value2, value1 / value2, value1 % value2);
+  alert("Addition:", value1 + value2);
+  alert("Subtraction:", value1 - value2);
+  alert("Multiplication:", value1 * value2);
+  alert("Division:", value1 / value2);
+  alert("Modulo:", value1 % value2);
   console.log(value1 + value2, value1 - value2, value1 * value2, value1 / value2, value1 % value2);
 }
 else {
