5d2224a57c78ae1a84dc1fc62e8860304c87ceac
Dennis <depazdennis@gmail.com>
2024-02-14 03:52:47 -0800

    ex2 ch4

--- js/ex2.js
+++ js/ex2.js
@@ -9,23 +9,23 @@ const userUnits = Number(prompt("How many units did you complete?"));
 if ((Number.isInteger(userUnits)) && userUnits >= 0) {
    if ((userUnits >= 0) && (userUnits <=30)) {
       let gradeStanding = "Freshman";
-      alert(`Hello ${userName}\n Your grade standing is ${gradeStanding}`);
-      console.log(`Hello ${userName}n\ Your grade standing is ${gradeStanding}`);
+      alert(`Hello ${userName}\nYour grade standing is ${gradeStanding}`);
+      console.log(`Hello ${userName}\n Your grade standing is ${gradeStanding}`);
    }
    else if ((userUnits > 30) && (userUnits <= 60)) {
       let gradeStanding = "Sophomore";
-      alert(`Hello ${userName}n\ Your grade standing is ${gradeStanding}`);
-      console.log(`Hello ${userName}n\ Your grade standing is ${gradeStanding}`);
+      alert(`Hello ${userName}\nYour grade standing is ${gradeStanding}`);
+      console.log(`Hello ${userName}\n Your grade standing is ${gradeStanding}`);
    }
    else if ((userUnits > 60) && (userUnits <=90)) {
       let gradeStanding = "Junior";
-      alert(`Hello ${userName}n\ Your grade standing is ${gradeStanding}`);
-      console.log(`Hello ${userName}n\ Your grade standing is ${gradeStanding}`);
+      alert(`Hello ${userName}\nYour grade standing is ${gradeStanding}`);
+      console.log(`Hello ${userName}\n Your grade standing is ${gradeStanding}`);
    }
    else {
       let gradeStanding = "Senior"
-      alert(`Hello ${userName}n\ Your grade standing is ${gradeStanding}`);
-      console.log(`Hello ${userName}n\ Your grade standing is ${gradeStanding}`);
+      alert(`Hello ${userName}n\Your grade standing is ${gradeStanding}`);
+      console.log(`Hello ${userName}\n Your grade standing is ${gradeStanding}`);
    }
 }
 else {
