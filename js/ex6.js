33a3e028b09db3201136c4442115982405c57772
Dennis <depazdennis@gmail.com>
2024-02-14 11:05:22 -0800

    ex6 v6

--- js/ex6.js
+++ js/ex6.js
@@ -3,9 +3,9 @@
 */
 
 console.log("De Paz's Output from Tutorial 4 Multiplication Table");
+const userInput = Number(prompt("Please enter a number:"));
 
 for (let i = 0; i < 11; i++) {
-  const userInput = Number(prompt("Please enter a number:"));
   if (Number.isInteger(userInput)) {
     alert(`${userInput} * ${i} = ${userInput * i}`);
     console.log(`${userInput} * ${i} = ${userInput * i}`);
